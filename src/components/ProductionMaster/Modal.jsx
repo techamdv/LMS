97f33c8baff999302622fe9d0411cd62766cd550
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-11-23 17:34:14 +0530

    test2 change

--- src/components/ProductionMaster/Modal.jsx
+++ src/components/ProductionMaster/Modal.jsx
@@ -23,26 +23,28 @@ function Modal() {
                         </div>
                         <div className="modal-body">
                             <div className="row">
-                                <div className="col-md-6">
+                                <div className="col-md-4">
                                     <div className="form-floating mb-3">
                                         <select className="form-select" id="floatingSelect1" aria-label="Floating label select example">
-                                            <option selected="">Plant name</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
+                                            <option selected="">Linamar</option>
                                         </select>
                                         <label htmlFor="floatingSelect1">Plant name</label>
                                     </div>
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-4">
                                     <div className="form-floating mb-3">
                                         <select className="form-select" id="floatingSelect2" aria-label="Floating label select example">
-                                            <option selected="">Line name</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
+                                            <option selected="">Cylinder head</option>
                                         </select>
-                                        <label htmlFor="floatingSelect2">Works with selects</label>
+                                        <label htmlFor="floatingSelect2">Line name</label>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="form-floating mb-3">
+                                        <select className="form-select" id="floatingSelect2" aria-label="Floating label select example">
+                                            <option selected="">Cylinder head</option>
+                                        </select>
+                                        <label htmlFor="floatingSelect2">Line name</label>
                                     </div>
                                 </div>
                             </div>
