e2be00e35d11d35323c6443b524a54e204e7e94d
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-11-23 16:56:24 +0530

    refracted code

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Dashboard from "./components/Dashboard";
 import Header from './components/Header';
 import SiderBar from './components/SiderBar';
 import Footer from './components/Footer';
-<<<<<<< HEAD
+
 import { Outlet, Route, Routes } from 'react-router-dom';
 import Test from './components/Test';
-=======
+
 import DownTimeEntry from './components/ProductionMaster/DownTimeEntry';
 
->>>>>>> 3d40e3258951700364fffcbf5407fbb1ce5d6439
 
 function App() {
   return (
@@ -18,17 +17,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/test' element={<Test />} />
+        <Route path='/downtime-entry' element={<DownTimeEntry />} />
       </Routes>
       <Header />
       <SiderBar />
-<<<<<<< HEAD
       <Outlet />
-=======
-      <Dashboard />
-      <DownTimeEntry/>
->>>>>>> 3d40e3258951700364fffcbf5407fbb1ce5d6439
       <Footer />
-      
+
 
     </>
   )
